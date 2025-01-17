da8110715f389079f52d48fc83f3f10b0570e2d8
Виктор Сукочев <vsukochev@yandex.ru>
2025-01-17 17:28:51 +0100

    bloc test

---
+++ test/UI/RssFeed/rss_feed_bloc_test.dart
@@ -0,0 +1,32 @@
+import 'package:rssfeeds/Model/feed_item.dart';
+import 'package:rssfeeds/Services/Feed/feed_service_api.dart';
+import 'package:rssfeeds/UI/RssFeed/rss_feed_bloc.dart';
+import 'package:rssfeeds/UI/RssFeed/rss_feed_event.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Test RSS feed bloc', () {
+    test('On receiving fetch event we should receive state with items', () {
+      final depsProviderStub = RssFeedBlocDepsProviderStub();
+      final bloc = RssFeedBloc(depsProviderStub);
+      bloc.stream
+          .listen(expectAsync1((value) => expect(value.items.length, 1)));
+      bloc.add(RssFeedFetchEvent());
+    });
+  });
+}
+
+final class FeedServiceStub implements FeedServiceApi {
+  @override
+  Future<List<FeedItem>> fetch() {
+    final itemsList = [
+      FeedItem(title: "Test title", textContent: "Test content"),
+    ];
+    return Future.value(itemsList);
+  }
+}
+
+final class RssFeedBlocDepsProviderStub implements RssFeedBlocDepsProvider {
+  @override
+  FeedServiceApi getFeedService() => FeedServiceStub();
+}
