b61a3abd73caeeed4cb3577bfe4598788c6029ab
Виктор Сукочев <vsukochev@yandex.ru>
2025-02-08 07:29:42 +0100

    Basic content navigation

--- lib/UI/RssFeed/FeedItemDetails/rss_feed_item_details_widget.dart
+++ lib/UI/RssFeed/FeedItemDetails/rss_feed_item_details_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:rssfeeds/Model/feed_item.dart';
+import 'package:rssfeeds/UI/RssFeed/FeedItemReader/rss_feed_item_reader_widget.dart';
 
 final class RssFeedItemDetailsWidget extends StatelessWidget {
   final FeedItem feedItem;
@@ -9,14 +10,20 @@ final class RssFeedItemDetailsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          feedItem.title,
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        Html(data: feedItem.textContent),
-      ],
+    return GestureDetector(
+      child: Column(
+        children: [
+          Text(
+            feedItem.title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Html(data: feedItem.textContent),
+        ],
+      ),
+      onTap: () =>
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+        return RssFeedItemReaderWidget(feedItem: feedItem);
+      })),
     );
   }
 }
---
+++ lib/UI/RssFeed/FeedItemReader/rss_feed_item_reader_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:rssfeeds/Model/feed_item.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+final class RssFeedItemReaderWidget extends StatelessWidget {
+  final FeedItem feedItem;
+
+  const RssFeedItemReaderWidget({super.key, required this.feedItem});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = WebViewController();
+    controller.loadRequest(feedItem.url);
+
+    return WebViewWidget(controller: controller);
+  }
+}
